f197a318816d19422a858d34a4e286d7159b9827
Pradeep3122003 <pradeepkh312@gmail.com>
2025-11-03 19:54:44 +0530

    simple calculator improved

--- Calculator.java
+++ Calculator.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter first number: ");
+        // System.out.print("Enter first number: ");
         double num1 = scanner.nextDouble();
-        System.out.print("Enter operation: ");
+        // System.out.print("Enter operation: ");
         char operation = scanner.next().charAt(0);
-        System.out.print("Enter second number: ");
+        // System.out.print("Enter second number: ");
         double num2 = scanner.nextDouble();
 
         switch (operation) {
